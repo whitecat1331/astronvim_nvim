08b02ca0ec00323b9a6d7374d4667d87042e805c
whitecat1331 <whitecat1331@gmail.com>
2024-08-22 17:54:32 -0400

    Update ctrl keyboard movement to alt

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -21,20 +21,31 @@ return {
           -- this is useful for naming menus
           ["<Leader>b"] = { name = "Buffers" },
           -- quick save
+          -- personal config
           ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
-          ["<C-s"] = ":w<cr>",
-          ["<C-k>"] = "zz<C-u>",
-          ["<C-j>"] = "zz<C-d>",
+          ["<A-k>"] = "zz<C-u>",
+          ["<A-j>"] = "zz<C-d>",
           ["<A-o>"] = "o<esc>",
           ["<A-O>"] = "O<esc>",
         },
         t = {
           -- setting a mapping to false will disable it
           -- ["<esc>"] = false,
+          --
+          ["<A-k>"] = "zz<C-u>",
+          ["<A-j>"] = "zz<C-d>",
+          ["<A-o>"] = "o<esc>",
+          ["<A-O>"] = "O<esc>",
         },
         v = {
           ["S"] = { ":s///g<left><left><left>", desc = "Find and Replace" },
           ["p"] = { '"_dP', desc = "Paste and re-copy to nvim register" },
+          -- ["<C-k>"] = "zz<C-u>",
+          -- ["<C-j>"] = "zz<C-d>",
+          ["<A-k>"] = "zz<C-u>",
+          ["<A-j>"] = "zz<C-d>",
+          ["<A-o>"] = "o<esc>",
+          ["<A-O>"] = "O<esc>",
         },
       },
     },
